a2c87b131c3399765622d58ffe16b171bfbc9e80
LuisRodriguezLopez94 <luis.rodriguez.sys@gmail.com>
2020-08-28 01:40:38 -0500

    aa

--- notificaciones/notificacion.php
+++ notificaciones/notificacion.php
@@ -6,6 +6,29 @@
 	$body = $_POST;
 	$data = json_decode($body);
 	http_response_code(200); // Return 200 OK 
-	echo $data;
-	echo $body;
+
+	if ($data->action == 'payment.created'){
+	  $msg = "Tu pago ha sido creado.";
+	  mail("luis.rodriguez.sys@gmail.com.com","Pago creado",$body);
+	}
+
+	if ($data->action == 'payment.updated'){
+	  $msg = "Tu pago ha sido creado.";
+	  mail("luis.rodriguez.sys@gmail.com.com","Pago creado",$body);
+
+	  	switch($data->type) {
+	        case "payment":
+	            $payment = MercadoPago\Payment.find_by_id($data->data.id);
+	            break;
+	        
+    	}
+
+	}
+
+
+	if ($data->action == 'test.created'){
+	  
+	  mail("luis.rodriguez.sys@gmail.com.com","Pago creado",$body);
+	}
+	
 ?>
