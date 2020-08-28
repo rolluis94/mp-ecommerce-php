e7490a06837b6b12bd7e9071210c3be7d64f70d2
LuisRodriguezLopez94 <luis.rodriguez.sys@gmail.com>
2020-08-28 02:36:21 -0500

    aas

--- php/mpcheckout.php
+++ php/mpcheckout.php
@@ -5,74 +5,86 @@
 	CONST INTEGRATOR_ID = "dev_24c65fb163bf11ea96500242ac130004";
 
 	function getReference($id_producto,$nombre_producto,$descripcion_producto,$url_imagen,$total){
+		try{
 
-		$external_reference = "luis.rodriguez.sys@gmail.com";
-		// Agrega credenciales
-		MercadoPago\SDK::setAccessToken(ACCESSTOKEN);
-		MercadoPago\SDK::setIntegratorId(INTEGRATOR_ID);
+			$external_reference = "luis.rodriguez.sys@gmail.com";
+			// Agrega credenciales
+			MercadoPago\SDK::setAccessToken(ACCESSTOKEN);
+			MercadoPago\SDK::setIntegratorId(INTEGRATOR_ID);
 
-		// Crea un objeto de preferencia
-		$preference = new MercadoPago\Preference();
+			// Crea un objeto de preferencia
+			$preference = new MercadoPago\Preference();
 
 
 
-		//crear comprador (payer)
-		$payer = new MercadoPago\Payer();
-		$payer->name = "Lalo";
-		$payer->surname = "Landa";
-		$payer->email = "test_user_58295862@testuser.com";
-		$payer->phone = array(
-		"area_code" => "52",
-		"number" => "5549737300"
-		);
+			//crear comprador (payer)
+			$payer = new MercadoPago\Payer();
+			$payer->name = "Lalo";
+			$payer->surname = "Landa";
+			$payer->email = "test_user_58295862@testuser.com";
+			$payer->phone = array(
+			"area_code" => "52",
+			"number" => "5549737300"
+			);
 
-		$payer->address = array(
-		"street_name" => "Insurgentes Sur",
-		"street_number" => 1602,
-		"zip_code" => "0394​0"
-		);
+			$payer->address = array(
+			"street_name" => "Insurgentes Sur",
+			"street_number" => 1602,
+			"zip_code" => "0394​0"
+			);
 
-		//agregamos a la referencia
-		$preference->payer = $payer;
-		echo $external_reference;
-		//crear Producto (item)
-		$item = new MercadoPago\Item();
-		$item->id = $id_producto;
-		$item->title = $nombre_producto;
-		$item->description = $descripcion_producto;
-		$item->picture_url = $url_imagen;
-		$item->quantity = 1;
-		$item->external_reference = $external_reference;
-		$item->currency_id = "MXN";
-		$item->unit_price = $total;
+			//agregamos a la referencia
+			$preference->payer = $payer;
 
-		//agregamos a la referencia
-		$preference->items = array($item);
+			//crear Producto (item)
+			$item = new MercadoPago\Item();
+			$item->id = $id_producto;
+			$item->title = $nombre_producto;
+			$item->description = $descripcion_producto;
+			$item->picture_url = $url_imagen;
+			$item->quantity = 1;
+			$item->external_reference = $external_reference;
+			$item->currency_id = "MXN";
+			$item->unit_price = $total;
 
-		//Excluimos American Express y atm y 6 cuotas
-		$preference->payment_methods = array(
-		  "excluded_payment_methods" => array(
-		    array("id" => "amex")
-		  ),
-		  "excluded_payment_types" => array(
-		    array("id" => "atm")
-		  ),
-		  "installments" => 6
-		);
+			//agregamos a la referencia
+			$preference->items = array($item);
+			
+			echo $external_reference;
+
+			//Excluimos American Express y atm y 6 cuotas
+			$preference->payment_methods = array(
+			  "excluded_payment_methods" => array(
+			    array("id" => "amex")
+			  ),
+			  "excluded_payment_types" => array(
+			    array("id" => "atm")
+			  ),
+			  "installments" => 6
+			);
+			
+
+			//back_urls
+			$preference->back_urls = array(
+			    "success" => "https://rolluis94-mp-ecommerce-php.herokuapp.com/pay/aprovado.php",
+			    "failure" => "https://rolluis94-mp-ecommerce-php.herokuapp.com/pay/rechazado.php",
+			    "pending" => "https://rolluis94-mp-ecommerce-php.herokuapp.com/pay/pendiente.php"
+			);
+			$preference->auto_return = "approved";
+
+			//webhook
+			$preference->notification_url = "https://rolluis94-mp-ecommerce-php.herokuapp.com/notificaciones/notificacion.php";
+
+			$preference->save();
+
+			return $preference;
+
+		}catch (Exception $e) {
+
+			echo json_encode($e);
+
+		}
+
 		
-
-		//back_urls
-		$preference->back_urls = array(
-		    "success" => "https://rolluis94-mp-ecommerce-php.herokuapp.com/pay/aprovado.php",
-		    "failure" => "https://rolluis94-mp-ecommerce-php.herokuapp.com/pay/rechazado.php",
-		    "pending" => "https://rolluis94-mp-ecommerce-php.herokuapp.com/pay/pendiente.php"
-		);
-		$preference->auto_return = "approved";
-
-		//webhook
-		$preference->notification_url = "https://rolluis94-mp-ecommerce-php.herokuapp.com/notificaciones/notificacion.php";
-
-		$preference->save();
-		return $preference;
 	}
 ?>
